da2624c9d1803d3fade4de159fc4309ba0d71041
PreethiIT47 <preethiselvakumar42@gmail.com>
2025-02-23 20:33:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import About from './components/About'; // Import the About component
+import About from './components/About';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Home" element={<Home />} /> {/* Home Page */}
-        <Route path="/login" element={<Login />} /> {/* Login Page */}
-        <Route path="/signup" element={<Signup />} /> {/* Signup Page */}
-        <Route path="/about" element={<About />} /> {/* About Page */}
+        <Route path="/" element={<Home />} /> {/* Default route for Home */}
+        <Route path="/home" element={<Home />} /> {/* Keep this if needed */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
